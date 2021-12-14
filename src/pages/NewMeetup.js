98e9dc092418c70ca7a492b0f07d042a35273e61
Joe Giuffrida <conquercat24@gmail.com>
2021-12-13 17:21:46 -0800

    added NewMeetupForm component and rendered it on the add new meetup page

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,11 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 const NewMeetupPage = () => {
   return (
-    <div>
-      <p>New Meetup Page</p>
-    </div>
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
   );
 };
 
