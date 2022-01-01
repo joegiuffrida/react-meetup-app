c9686e5a1b987a0d06e4e639b6b16eff0802d9b5
Joe Giuffrida <conquercat24@gmail.com>
2021-12-31 18:01:32 -0800

    added functions to context

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -3,14 +3,36 @@ import { createContext, useState } from 'react';
 const FavoritesContext = createContext({
   favorites: [],
   totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {},
 });
 
-const FavoritesContextProvider = ({ children }) => {
+export const FavoritesContextProvider = ({ children }) => {
   const [userFavorites, setUserFavorites] = useState([]);
+
+  const addFavoriteHandler = (favoriteMeetup) => {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  };
+
+  const removeFavoriteHandler = (meetupId) => {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  };
+
+  const itemIsFavoriteHandler = (meetupId) => {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  };
 
   const context = {
     favorites: userFavorites,
     totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
   };
 
   return (
@@ -19,3 +41,5 @@ const FavoritesContextProvider = ({ children }) => {
     </FavoritesContext.Provider>
   );
 };
+
+export default FavoritesContext;
